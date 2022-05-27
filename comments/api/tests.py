26e52e2b4200c38e51434a7dbf73facdd19c795b
yw1101 <617201167@qq.com>
2022-05-27 02:39:26 -0400

    Add unit test and list function for comments api

--- comments/api/tests.py
+++ comments/api/tests.py
@@ -103,3 +103,29 @@ class CommentApiTests(TestCase):
         self.assertEqual(comment.created_at, before_created_at)
         self.assertNotEqual(comment.created_at, now)
         self.assertNotEqual(comment.updated_at, before_updated_at)
+
+    def test_list(self):
+        response = self.anonymous_client.get(COMMENT_URL)
+        self.assertEqual(response.status_code, 400)
+
+        response = self.anonymous_client.get(COMMENT_URL, {
+            'tweet_id': self.tweet.id,
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['comments']), 0)
+
+        self.create_comment(self.kellynim, self.tweet, '1')
+        self.create_comment(self.talenti, self.tweet, '2')
+        self.create_comment(self.talenti, self.create_tweet(self.talenti), '3')
+        response = self.anonymous_client.get(COMMENT_URL, {
+            'tweet_id': self.tweet.id,
+        })
+        self.assertEqual(len(response.data['comments']), 2)
+        self.assertEqual(response.data['comments'][0]['content'], '1')
+        self.assertEqual(response.data['comments'][1]['content'], '2')
+
+        response = self.anonymous_client.get(COMMENT_URL, {
+            'tweet_id': self.tweet.id,
+            'user_id': self.kellynim.id,
+        })
+        self.assertEqual(len(response.data['comments']), 2)
--- comments/api/views.py
+++ comments/api/views.py
@@ -13,6 +13,7 @@ from comments.api.permissions import IsObjectOwner
 class CommentViewSet(viewsets.GenericViewSet):
     serializer_class = CommentSerializerForCreate
     queryset = Comment.objects.all()
+    filterset_fields = ('tweet_id',)
 
     def get_permissions(self):
         if self.action == 'create':
@@ -20,6 +21,23 @@ class CommentViewSet(viewsets.GenericViewSet):
         if self.action in ['update', 'destroy']:
             return [IsAuthenticated(), IsObjectOwner()]
         return [AllowAny()]
+
+    def list(self, request, *args, **kwargs):
+        #id is in the url
+        if "tweet_id" not in request.query_params:
+            return Response({
+                'message': 'missing tweet_id in request',
+                'success': False,
+            }, status = status.HTTP_400_BAD_REQUEST)
+        queryset = self.get_queryset()
+        comments = self.filter_queryset(queryset).prefetch_related('user').order_by('created_at')
+        serializer = CommentSerializer(comments, many = True)
+        return Response({
+            'comments': serializer.data},
+            status = status.HTTP_200_OK
+        )
+        #make sure to return the hash map
+
 
     def create(self, request, *args, **kwargs):
         data = {
