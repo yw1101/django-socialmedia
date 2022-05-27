705524d4928a248bc868103ddfe44fb28c7e7641
yw1101 <617201167@qq.com>
2022-05-26 20:48:36 -0400

    delete comments preparing recoding it

--- comments/models.py
+++ comments/models.py
@@ -4,14 +4,21 @@ from tweets.models import Tweet
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    tweet = models.ForeignKey(Tweet, null=True, on_delete=models.SET_NULL)
-    content = models.TextField(max_length=140)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(
+        User,
+        null=True,
+        on_delete = models.SET_NULL
+    )
+    tweet = models.ForeignKey(
+        Tweet,
+        null = True,
+        on_delete = models.SET_NULL
+    )
+    content = models.TextField(max_length = 140)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
 
     class Meta:
-        # 有在某个 tweet 下排序所有 comments 的需求
         index_together = (('tweet', 'created_at'),)
 
     def __str__(self):
