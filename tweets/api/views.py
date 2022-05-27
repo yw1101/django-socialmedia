37b3e9bba12c1b2940cf7904acdc1f300048d162
yw1101 <617201167@qq.com>
2022-05-27 04:20:58 -0400

    Get tweet with comments

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -1,35 +1,44 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from tweets.api.serializers import TweetSerializer, TweetSerializerForCreate
+from tweets.api.serializers import (
+    TweetSerializer,
+    TweetSerializerForCreate,
+    TweetSerializerWithComments
+)
 from tweets.models import Tweet
 from newsfeeds.services import NewsFeedService
+from utils.decorators import required_params
+
 
 class TweetViewSet(viewsets.GenericViewSet):
     serializer_class = TweetSerializerForCreate
     queryset = Tweet.objects.all()
 
     def get_permissions(self):
-        if self.action == 'list':
+        if self.action in ['list', 'retrieve']:
             #Can access without logging in
             return [AllowAny()]
         #Need to log in to access
         return [IsAuthenticated()]
 
-    #Match self.action
-    def list(self, request, *args, **kwargs):
-        if 'user_id' not in request.query_params:
-            return Response('missing user_id', status = 400)
+    def retrieve(self, request, *args, **kwargs):
+        tweet = self.get_object()
+        return Response(TweetSerializerWithComments(tweet).data)
 
+    #Match self.action
+    @required_params(params = ['user_id'])
+    def list(self, request, *args, **kwargs):
+        user_id = request.query_params['user_id']
         #select * from twitter_tweets
         #where user_id = xxx
         #order by created_at desc
-        tweets = Tweet.objects.filter(
-            user_id=request.query_params['user_id']
-        ).order_by('-created_at')
+        tweets = Tweet.objects.filter(user_id = user_id).order_by('-created_at')
         #tweets is the query set transfered
         serializer = TweetSerializer(tweets, many = True) #return list of dict
         return Response({'tweets': serializer.data})
+
+
 
     def create(self, request, *args, **kwargs):
         serializer = TweetSerializerForCreate(
