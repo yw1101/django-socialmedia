a732262800d957e231f4b2d9841367d094f32a97
yw1101 <617201167@qq.com>
2022-05-30 18:28:50 -0400

    Add the update api

--- inbox/api/views.py
+++ inbox/api/views.py
@@ -1,9 +1,13 @@
-from inbox.api.serializers import NotificationSerializer
+from inbox.api.serializers import (
+    NotificationSerializer,
+    NotificationSerializerForUpdate,
+)
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from notifications.models import Notification
+from utils.decorators import required_params
 
 
 class NotificationViewSet(
@@ -29,4 +33,21 @@ class NotificationViewSet(
         return Response(
             {'marked_count': updated_count},
             status = status.HTTP_200_OK
+        )
+
+    @required_params(method = 'POST', params = ['unread'])
+    def update(self, request, *args, **kwargs):
+        serializer = NotificationSerializerForUpdate(
+            instance = self.get_object(),
+            data = request.data,
+        )
+        if not serializer.is_valid():
+            return Response({
+                'message': 'Please check input.',
+                'errors': serializer.erros,
+            }, status = status.HTTP_400_BAD_REQUEST)
+        notification = serializer.save()
+        return Response(
+            NotificationSerializer(notification).data,
+            status = status.HTTP_200_OK,
         )
