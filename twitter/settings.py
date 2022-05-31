d73a64342e2e4aa3cede2439bf4810df5cdc4551
yw1101 <617201167@qq.com>
2022-05-31 02:48:59 -0400

    Set up the Amazon S3

--- twitter/settings.py
+++ twitter/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import sys
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -142,9 +144,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+
 STATIC_URL = '/static/'
 
-# try:
-#     from .local_settings import *
-# except:
-#     pass
+MEDIA_ROOT = 'media/'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+TESTING = ((" ".join(sys.argv)).find('manage.py test') != -1)
+if TESTING:
+    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
+AWS_STORAGE_BUCKET_NAME = 'django-socialmedia'
+AWS_S3_REGION_NAME = 'us-east-1'
+
+
+try:
+    from .local_settings import *
+except:
+    pass
