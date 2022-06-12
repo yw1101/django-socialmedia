14cd42d8acecbf08eee4b9195eda99b95cf4158d
yw1101 <617201167@qq.com>
2022-06-12 03:26:28 -0400

    Add pagination in utils

--- friendships/api/serializers.py
+++ friendships/api/serializers.py
@@ -3,26 +3,45 @@ from friendships.models import Friendship
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from django.contrib.auth.models import User
+from friendships.services import FriendshipService
 
 
 class FollowerSerializer(serializers.ModelSerializer):
     user = UserSerializerForFriendship(source = 'from_user')
+    has_followed = serializers.SerializerMethodField()
     created_at = serializers.DateTimeField()
 
     class Meta:
         model = Friendship
         #user is actually the from_user
-        fields = ('user', 'created_at')
+        fields = ('user', 'created_at', 'has_followed')
+
+    def get_has_followed(self, obj):
+        if self.context['request'].user.is_anonymous:
+            return False
+        return FriendshipService.has_followed(
+            self.context['request'].user,
+            obj.from_user
+        )
 
 
 class FollowingSerializer(serializers.ModelSerializer):
     user = UserSerializerForFriendship(source = 'to_user')
     created_at = serializers.DateTimeField()
+    has_followed = serializers.SerializerMethodField()
 
     class Meta:
         model = Friendship
         #user is actually the from_user
-        fields = ('user', 'created_at')
+        fields = ('user', 'created_at', 'has_followed')
+
+    def get_has_followed(self, obj):
+        if self.context['request'].user.is_anonymous:
+            return False
+        return FriendshipService.has_followed(
+            self.context['request'].user,
+            obj.from_user
+        )
 
 class FriendshipSerializerForCreate(serializers.ModelSerializer):
     from_user_id = serializers.IntegerField()
--- friendships/api/views.py
+++ friendships/api/views.py
@@ -9,32 +9,38 @@ from friendships.api.serializers import(
     FriendshipSerializerForCreate,
 )
 from django.contrib.auth.models import User
+from utils.paginations import EndlessPagination
 
 
 class FriendshipViewSet(viewsets.GenericViewSet):
     serializer_class = FriendshipSerializerForCreate
     queryset = User.objects.all()
+    pagination_class = EndlessPagination
 
     @action(methods = ['GET'], detail = True, permission_classes = [AllowAny])
     def followers(self, request, pk):
         #GET /api/friendships/1/followers/
         friendships = Friendship.objects.filter(to_user_id = pk).order_by('-created_at')
-        serializer = FollowerSerializer(friendships, many = True)
-        return Response(
-            {'followers': serializer.data},
-            status = status.HTTP_200_OK,
+        page = self.paginate_queryset(friendships)
+        serializer = FollowerSerializer(
+            page,
+            many = True,
+            context = {'request': request}
         )
+        return self.get_paginated_response(serializer.data)
 
 
     @action(methods = ['GET'], detail = True, permission_classes = [AllowAny])
     def followings(self, request, pk):
         #GET /api/friendships/1/followers/
         friendships = Friendship.objects.filter(from_user_id = pk).order_by('-created_at')
-        serializer = FollowingSerializer(friendships, many = True)
-        return Response(
-            {'followings': serializer.data},
-            status = status.HTTP_200_OK,
+        page = self.paginate_queryset(friendships)
+        serializer = FollowingSerializer(
+            page,
+            many = True,
+            context = {'request': request}
         )
+        return self.get_paginated_response(serializer.data)
 
 
     @action(methods = ['POST'], detail = True, permission_classes = [IsAuthenticated])
@@ -54,7 +60,7 @@ class FriendshipViewSet(viewsets.GenericViewSet):
             }, status = status.HTTP_400_BAD_REQUEST)
         instance = serializer.save()
         return Response(
-            FollowingSerializer(instance).data,
+            FollowingSerializer(instance, context = {'request': request}).data,
             status = status.HTTP_201_CREATED,
         )
 
