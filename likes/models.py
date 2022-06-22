e514665ad7d1200d6f361f5c14c1c509aa2811bd
yw1101 <617201167@qq.com>
2022-06-22 19:54:03 -0400

    Part in database part in cache and update the decrease, increase for count of the likes and comments

--- likes/models.py
+++ likes/models.py
@@ -3,6 +3,9 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from utils.memcached_helper import MemcachedHelper
+from django.db.models.signals import pre_delete, post_save
+from likes.listeners import incr_likes_count, decr_likes_count
+
 
 class Like(models.Model):
     object_id = models.PositiveIntegerField()
@@ -31,3 +34,6 @@ class Like(models.Model):
     @property
     def cached_user(self):
         return MemcachedHelper.get_object_through_cache(User, self.user_id)
+
+pre_delete.connect(decr_likes_count, sender = Like)
+post_save.connect(incr_likes_count, sender = Like)
